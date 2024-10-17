680d1e2a094ae637d1abd75d38626d1b3abcbf06
leonhatches <jhatches@unsa.edu.pe>
2024-10-16 19:37:32 -0500

    Commit de PRIMER ENUNCIADO - COMPLETO

--- Enunciado01.pl
+++ Enunciado01.pl
@@ -1,25 +1,28 @@
 #!/usr/bin/perl;
 
-# Escriba un programa de Perl que pida nombre de usuario y luego, dominio. El programa debera imprimir con el "@" en medio. Como restriccion, no se podra usar operadores de concatenacion.
+# Escriba un programa de Perl que pida nombre de usuario y luego, dominio. El programa
+# debera imprimir con el "@" en medio. Como restriccion, no se podra usar operadores de
+# concatenacion.
 
 	# TITULO
-	print "\t| CORREO ELECTRONICO |\n";
+	print "\n\t| CORREO ELECTRONICO |\n\n";
 	
 	# ENTRADA DE NOMBRE
-	print "Ingrese su nombre: ";
-	$nombre = <MTDIN>;
+	print "Ingrese su nombre:\t";
+	$nombre = <STDIN>;
 	chomp($nombre);
 	
 	# ENTRADA DE DOMINIO
-	print "\nIngrese el dominio: ";
-	$dominio = <MTDIN>;
+	print "Ingrese el dominio:\t";
+	$dominio = <STDIN>;
 	chomp($dominio);
 	
 	# SALIDA | IMPRIMIR
-	print "\n";
+	print "\nCORREO ELECTRONICO: ";
 	print $nombre;
 	print "@";
 	print $dominio;
+	print "\n\n";
 
 
 	
